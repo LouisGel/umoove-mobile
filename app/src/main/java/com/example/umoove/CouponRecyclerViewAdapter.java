85d11a6be4fd22b577d9c7b5d2543678e4c9d3f1
LouisGel <Louisxg@hotmail.com>
2024-02-04 08:14:26 -0500

    Capable d'acheter des coupons (seulement pour user 1)

--- app/src/main/java/com/example/umoove/CouponRecyclerViewAdapter.java
+++ app/src/main/java/com/example/umoove/CouponRecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.umoove;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,13 +14,19 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class CouponRecyclerViewAdapter extends RecyclerView.Adapter<CouponRecyclerViewAdapter.viewHolder> {
@@ -51,25 +58,123 @@ public class CouponRecyclerViewAdapter extends RecyclerView.Adapter<CouponRecycl
         holder.txtViewRemaining.setText(String.valueOf(coupons.get(position).getRemaining()));
 
 
+        int price = coupons.get(position).getPrice();
+
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 OkHttpClient client = new OkHttpClient();
-                String getURL = "https://umoove.co/api/activities";
+                OkHttpClient client2 = new OkHttpClient();
+                String getURL = "https://umoove.co/api/user/1";
+                String postURL = "https://umoove.co/api/buy";
 
-                Request req = new Request.Builder().url(getURL).build();
-                client.newCall(req).enqueue(new Callback() {
+                Request reqVerif = new Request.Builder().url(getURL).build();
+
+
+                JSONObject jsonBody = new JSONObject();
+                try {
+                    jsonBody.put("coupon_type_id", "1");
+                    jsonBody.put("user_id", "1");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), jsonBody.toString());
+
+                Request reqPayement = new Request.Builder().url(postURL).post(requestBody).build();
+
+                //RequestBody requestBody = new FormBody.Builder().add("coupon_type_id", "1").add("user_id", "1").build();
+                //Request reqPayement = new Request.Builder().url(postURL).post(requestBody).build();
+
+
+
+
+                client.newCall(reqVerif).enqueue(new Callback() {
                     @Override
                     public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                        Toast.makeText(view.getContext(), "Clic sur l'élément à la position " + position, Toast.LENGTH_SHORT).show();
+                        ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(view.getContext(), "Une erreur est survenue, veuillez réssayer", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
 
                     }
 
                     @Override
-                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    public void onResponse(@NonNull Call call, @NonNull Response responseVerif) throws IOException {
 
-                        String x = response.body().string();
-                        Toast.makeText(view.getContext(), response.body().string(), Toast.LENGTH_SHORT).show();
+
+                        String responseVerifStr = responseVerif.body().string();
+
+                        try {
+                            JSONObject jObject = new JSONObject(responseVerifStr);
+
+                            int portefeuille = jObject.getInt("points");
+                            if (portefeuille < price){
+                                ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(view.getContext(), "Vous n'avez pas assez de points pour acheter ce coupon...", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(view.getContext(), "Une erreur est survenue, veuillez réssayer", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            return;
+                        }
+
+
+
+                        //=========================================================Paye le coupon
+                        client2.newCall(reqPayement).enqueue(new Callback() {
+                            @Override
+                            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                                ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(view.getContext(), "Une erreur est survenue, veuillez réssayer", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onResponse(@NonNull Call call, @NonNull Response responsePaiement) throws IOException {
+                                String responsePaiementStr = responsePaiement.body().string();
+
+                                try {
+                                    JSONObject jObject = new JSONObject(responsePaiementStr);
+                                        ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                try {
+                                                    String code = jObject.getString("code");
+                                                    Toast.makeText(view.getContext(), "Wow! Votre code rabais est : " + code, Toast.LENGTH_SHORT).show();
+                                                } catch (JSONException e) {
+                                                    throw new RuntimeException(e);
+                                                }
+                                            }
+                                        });
+                                } catch (JSONException e) {
+                                    ((Activity) view.getContext()).runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(view.getContext(), "Une erreur est survenue, veuillez réssayer", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            }
+                        });
 
                     }
                 });
